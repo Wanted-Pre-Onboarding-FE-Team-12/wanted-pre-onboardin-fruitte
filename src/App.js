da70836baee93f759555d1343c42f946fb10703c
Jaehoon Kim <evolui@naver.com>
2022-09-03 23:33:57 +0900

    feat: 추가 관리페이지 관리페이지 초안

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { useEffect } from 'react';
 import Admin from '@pages/Admin';
 import List from '@pages/List';
 import ProductMg from '@pages/ProductMg';
 import AddProduct from '@pages/AddProduct';
+import { useRecoilState } from 'recoil';
+import { productState } from 'src/store/store';
 
 function App() {
+  const [, setProduct] = useRecoilState(productState);
+
+  useEffect(() => {
+    const fetchingData = async () => {
+      const result = await fetch('/data/product.json');
+      const promise = result.json();
+      promise.then(res => setProduct(res));
+    };
+    fetchingData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,12 @@
 import Layout from '@layouts/index';
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { productState } from 'src/store/store';
 
 const List = () => {
+  const [product] = useRecoilState(productState);
+
+  console.log(product);
   return <Layout>List</Layout>;
 };
 
