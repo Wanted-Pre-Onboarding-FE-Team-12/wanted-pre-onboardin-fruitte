dd56ad6e0df314db44680a7a037e15202c833679
bbbb <bgh9651@gmail.com>
2022-09-04 19:28:26 +0900

    main pull

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import { useEffect } from 'react';
-=======
->>>>>>> 799f941 (PR 피드백 수정)
 import Admin from '@pages/Admin';
 import List from '@pages/List';
 import AdminProduct from '@pages/AdminProduct';
 import AddProduct from '@pages/AddProduct';
-<<<<<<< HEAD
 import { useRecoilState } from 'recoil';
 import { productState } from 'src/store/store';
-import OrderConfirm from '@components/OrderConfirm';
-=======
 import OrderConfirm from '@pages/OrderConfirm';
->>>>>>> 799f941 (PR 피드백 수정)
 
 function App() {
   const [, setProduct] = useRecoilState(productState);
