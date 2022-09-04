32e8299214ed79c80c5fb526ffa6e31d0c587db8
jinwoo <wwo1278@gmail.com>
2022-09-04 16:16:26 +0900

    feat: 페이지네이션
    1. 테이블에서 사용할 페이지네이션 추가하였습니다.
    2. 기존 테이블 컴포넌트 헤더와 바디가 나눠있던걸 합쳤습니다.
    3.

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
+
+/**
+ * @param {Array} titleArr - 테이블의 헤더 항목
+ * @param {Array} product - 현재 페이지에서 표현할 데이터
+ * @param {(id)=>{}} deleteProduct - 데이터 삭제 함수
+ * @param {(id)=>{}} setVisibleProduct - 데아터 보이기 여부 변경 함수
+ * @return {React.Component}
+ */
+const Table = props => {
+  const { titleArr, product, deleteProduct, setVisibleProduct } = props;
+
+  return (
+    <StyledTable>
+      <thead>
+        <TableHeader titleArr={titleArr} />
+      </thead>
+      <tbody>
+        <TableBody
+          product={product}
+          deleteProduct={deleteProduct}
+          setVisibleProduct={setVisibleProduct}
+        />
+      </tbody>
+    </StyledTable>
+  );
+};
+
+const StyledTable = styled.table``;
+
+export default Table;
